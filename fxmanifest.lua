2405a612ad9b1ac0f9a61f7c91d9f3378d033b11
gta multiverse rp <gta-multiverse-rp@fx.server>
2025-03-03 17:04:42 +0900

    cdn-fuel導入。streamをcdn-fuel-dataに移動

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -22,7 +22,7 @@ server_scripts {
 shared_scripts {
     'shared/config.lua',
     '@qb-core/shared/locale.lua',
-    -- '@ox_lib/init.lua', -- OX_Lib, only line this in if you have ox_lib and are using them.
+    '@ox_lib/init.lua', -- OX_Lib, only line this in if you have ox_lib and are using them.
     'locales/en.lua', -- English Locales
     -- 'locales/de.lua', -- German / Deutsch Locales
     -- 'locales/fr.lua', -- French / Français Locales
@@ -41,15 +41,12 @@ dependencies { -- Make sure these are started before cdn-fuel in your server.cfg
     'PolyZone',
     'interact-sound',
     -- QB-Core Functionality (Input, Target, Menu)
-    'qb-target',
+    --'qb-target',
     'qb-input',
     'qb-menu',
     -- QBox | Overextended Functionalities (Input, Progressbar, Target, Menu etc.)
-    -- 'ox_lib', -- Ox Library
-    -- 'ox_target',
+     'ox_lib', -- Ox Library
+     'ox_target',
 }
-
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_charger]/electric_charger_typ.ytyp'
 
 provide 'cdn-syphoning' -- This is used to override cdn-syphoning(https://github.com/CodineDev/cdn-syphoning) if you have it installed. If you don't have it installed, don't worry about this. If you do, we recommend removing it and using this instead.
